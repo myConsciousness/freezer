99d9bcce9569386c55d29acfa2f3fc4d07c726e8
myConsciousness <kato.shinya.dev@gmail.com>
2022-08-17 11:10:35 +0900

    test: delete empty file

--- test/freezer_test.dart
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 Kato Shinya. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided the conditions.
-
-import 'package:test/test.dart';
-
-void main() {
-  test('.', () {});
-}
